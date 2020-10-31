eab2674c7365ec4cdfaeead363be7e9f9c5dc600
Dana Kuglin <dakuglin@gmail.com>
2020-10-31 14:08:03 -0600

    updated files

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -7,17 +7,17 @@ var burger = require("../models/burger.js");
 //create the routes and set up logic within routes 
 router.get("/", function(req, res) {
 
-  burger.selectAll(function(data) {
-    var hbsObject = {
-        burger: data
-    };
-    console.log(hbsObject)
-    res.render("index", hbsObject);
-  });
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject)
+        res.render("index", hbsObject);
+    });
 });
 
 router.post("/api/burgers", function(req, res) {
-    burger.create([
+    burger.insertOne([
         "burger_name", "devoured"
     ], [
         req.body.burger_name, req.body.devoured
@@ -33,9 +33,9 @@ router.put("/api/burgers/:id", function(req, res) {
     console.log("condition", condition);
 
     burger.updateOne({
-        devoured : req.body.devoured
+        devoured: req.body.devoured
     }, condition, function(result) {
-        if (result.changedRows == 0) {
+        if (result.changedRows === 0) {
             return res.status(404).end();
         }
         else {
--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,12 +1,35 @@
 //need to wait to attach or handlebars until the DOM is fully loaded
 
 $(function() {
+    
+    $(".create-form").on("submit", function(e) {
+        e.preDefault();
+
+        //creates new burger
+        var newBurger = {
+            burger_name: $("#addburger").val().trim(),
+            devoured: 0
+        };
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("Successfully added a new burger!");
+                //reload page to get updated list
+                location.reload();
+            }
+        );
+    });
+
+
     $(".change-devoured").on("click", function(e) {
+        e.preDefault();
         var id = $(this).data("id");
-        var newDevour = $(this).data("newburger");
+        var devourState = 1;
 
         var newDevourState = {
-            devoured : newDevour
+            devoured : devourState
         },
         // Send the PUT request.
         $.ajax("/api/burgers/" + id, {
@@ -14,31 +37,11 @@ $(function() {
             data: newDevourState
         }).then(
             function() {
-            console.log("changed devoured to", newDevour);
+            console.log("changed devoured to", devourState);
             // Reload the page to get the updated list
             location.reload();
             }
         );
     });
 
-
-    $(".create-form").on("submit", function(e) {
-        e.preDefault();
-
-        var newBurger = {
-            burger_name: $("#addburger").val().trim(),
-            devoured: 0
-        };
-
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(
-            function() {
-                console.log("Successfully created a new burger!");
-                //reload page to get updated list
-                location.reload();
-            }
-        );
-    });
 });
